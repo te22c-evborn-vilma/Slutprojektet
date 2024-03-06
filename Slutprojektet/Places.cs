528da654215428a8c0bd46c936f7684dd3ffa215
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-03-06 11:14:43 +0100

    changes

--- Slutprojektet/Places.cs
+++ Slutprojektet/Places.cs
@@ -1,28 +1,40 @@
 // inspiration från Bad Structure-projektet
 
+using System.ComponentModel;
+
 class Places
 {
     public static string StartPlace(string position)
     {
-        Console.WriteLine("Det första jag ser när jag öppnar ögonen är blått. Efter ett tag inser jag att jag kollar på en himmel.\nMen det kan väl inte stämma. Det sista jag kommer ihåg var att jag... Vad var det sista jag gjorde nu igen?\nJag kommer inte ihåg någonting. Vad ska jag göra nu?");
-        Console.WriteLine("\n1. Ligga kvar på vad som känns som sand och fundera på vad som händer.\n2. Resa mig upp och kolla vart jag är.");
+        Console.WriteLine("Du ser en massa blått när du öppnar ögonen. Det är troligtvis en himmel.\nDu minns inte varför du kollar på en himmel.\nNär du tänker efter kommer du inte ihåg någonting.");
+        Console.WriteLine("\n1. Kolla i föremålet som ligger bredvid dig.\n2. Ställ dig upp.");
 
+        // Bad Structure-projektet  ------------------------------------------------------
         string choice = Console.ReadLine();
         choice = choice.ToLower();
+         // ------------------------------------------------------------------------------
 
         if (choice == "1")
         {
-            position = "layDown";
+            Console.WriteLine("Det verkar vara en ryggsäck med saker i.");
+            position = "checkBag";
         }
         else if (choice == "2")
         {
-            position = "sitUp";
+            position = "standUp";
         }
         else
         {
-            Console.WriteLine("Jag har inget annat val. Vad ska jag göra?\n");
+            Console.WriteLine("Du har inget annat val.");
             position = "start";
         }
+        return position;
+    }
+
+    public static string BeachPlace(string position)
+    {
+        
+        
         return position;
     }
 }
--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 /* ----------------------------------------------------------------------------
     VARIABLER 
 -----------------------------------------------------------------------------*/
@@ -17,34 +18,15 @@ while (position != "leave")
     }
 /* ----------------------------------------------------------------------------
     POSITION: BEACH
------------------------------------------------------------------------------*/
-    if (position == "layDown")
+-----------------------------------------------------------------------------*/  
+    if (position == "checkBag")
     {
-        Console.WriteLine("Varför ska jag ens försöka göra något? Lika bra att ge upp.");
-        position = "leave";
-    }
-    if (position == "sitUp")
-    {
-        Console.WriteLine("Hmmm... Det ser ut som att jag sitter på en strand, med havet som sträcker sig så långt jag kan se...Hmmm...Är den här ryggsäcken min? Den ligger ju bredvid mig.");
-        Console.WriteLine("\n1. Kolla vad som finns i ryggsäcken\n2. Resa mig upp");
-
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "1")
-        {
-            Bag();
-            // class eller metod??
-            position = "sitUp";
-        }
-        if (choice == "2")
-        {
-            position = "standUp";
-        }
+        Bag();
+        position = "start";
     }
     if (position == "standUp")
     {
-        Console.WriteLine("Wow! Stranden är så stor. Och djungeln bakom mig har så höga träd. Vad ska jag göra nu?");
+        Console.WriteLine("Du står på en strand som sträcker sig så långt ögat kan se åt båda håll. Framför dig ser du havet möta horisonten. Bakom dig finns en djungel med höga träd.");
         Console.WriteLine("1. Kolla i ryggsäcken\n2. Gå längs med stranden\n3. Gå och utforska djungeln");
 
         string choice = Console.ReadLine();
@@ -65,7 +47,7 @@ while (position != "leave")
     }
     if (position == "walkOnBeach")
     {
-        Console.WriteLine("Inget märkvärdigt att se förutom havet till höger och djungeln till vänster. Timmarna går. Jag fortsätter gå.\nSplash\nVad var det?");
+        Console.WriteLine("Du ser bara stranden framför dig, den verkar aldrig ta slut.Du fortsätter gå framåt.\nSplash\nEtt plötsligt ljud får dig att kolla dig omkring.");
         Console.WriteLine("1. Kolla i ryggsäcken\n2. Undersök vart ljudet kom ifrån\n3. Gå vidare");
 
         string choice = Console.ReadLine();
@@ -77,7 +59,7 @@ while (position != "leave")
         }
         if (choice == "2")
         {
-            position = "";
+            position = "exploreSound";
         }
         if (choice == "3")
         {
@@ -86,23 +68,32 @@ while (position != "leave")
     }
     if (position == "continueWalkOnBeach")
     {
-        Console.WriteLine("Efter ytterliggare några timmars promenerande ser jag hur sanden börjar röra sig under mig. Jag dyker åt sidan precis innan något hoppar upp ur sanden framför mig. Det ser ut som att... en jättekrabbe försöker attackera mig! Ånej, vad ska jag göra?");
-        Console.WriteLine("1. Kolla i ryggsäcken\n2. Dra fram mitt svärd från ryggsäcken och försvara mig\n3. Spring därifrån!!");
+        Console.WriteLine("Efter några timmars promenerande märker du hur sanden börjar röra på sig under dig. Du lyckas dyka åt sidan precis innan något hoppar upp ur sanden. Det ser ut som en... jättekrabba.");
+        Console.WriteLine("1. Kolla i ryggsäcken\n2. Dra fram svärdet från ryggsäcken och slå krabban.\n3. Spring därifrån.");
 
         string choice = Console.ReadLine();
         choice = choice.ToLower();
 
         if (choice == "1")
         {
-            Console.WriteLine("Jag hinner inte kolla i ryggsäcken innan jättekrabban attackerar mig med sina stora klor. I mina sista stunder funderar jag på vad jag skulle ha kunnat göra bättre.");
+            Console.WriteLine("Du hinner inte kolla i ryggsäcken innan jättekrabban attackerar dig med sina stora klor. I dina sista stunder funderar du på vad du skulle ha kunnat göra bättre.");
             position = "leave";
         }
-        
+        if (choice == "2")
+        {
+            Console.WriteLine("Du drar fram ditt svärd");
+            position = "fightCrab";
+        }
+        if (choice == "3")
+        {
+            Console.WriteLine("Du rusar därifrån, men upptäcker snabbt att bästa sättet att komma bort från krabban är att springa in i skogen.");
+            position = "hideInDjungle";
+        }
     }
 /* ----------------------------------------------------------------------------
     POSITION: WATER 
 -----------------------------------------------------------------------------*/
-    if (position == "")
+    if (position == "exploreSound")
     {
 
     }
@@ -113,11 +104,33 @@ while (position != "leave")
     {
 
     }
+    if (position == "hideInDjungle")
+    {
+        Console.WriteLine("");
+    }
 }
+/* ----------------------------------------------------------------------------
+    POSITION: FIGHT
+-----------------------------------------------------------------------------*/
+    if (position == "fightCrab")
+    {
+        Console.WriteLine("Slå på krabbans huvud eller en av dess klor.");
+        Console.ReadLine();
+        Fight();
+    }
+
 Console.ReadLine();
 
+// ändra till en bool eller liknande
+static void Fight()
+{
+
+}
 
 static void Bag()
 {
     Console.WriteLine("Min väska innehåller:");
 }
+
+
+// List<string> items = new List<string>();
