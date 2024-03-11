12f720a7d3530f98a1c55b1de15e1f640c1c1e11
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-03-11 09:28:56 +0100

    lista till väskan (kanske ändrar till array), ändrat svarsalternativen, flyttar på onödig kod i program.cs

--- Slutprojektet/Places.cs
+++ Slutprojektet/Places.cs
@@ -6,34 +6,82 @@ class Places
 {
     public static string StartPlace(string position)
     {
-        Console.WriteLine("Du ser en massa blått när du öppnar ögonen. Det är troligtvis en himmel.\nDu minns inte varför du kollar på en himmel.\nNär du tänker efter kommer du inte ihåg någonting.");
-        Console.WriteLine("\n1. Kolla i föremålet som ligger bredvid dig.\n2. Ställ dig upp.");
+        if (position == "start")
+        {
+            Console.WriteLine("Du ser en massa blått när du öppnar ögonen. Det är troligtvis en himmel.\nDu minns inte varför du kollar på en himmel.\nNär du tänker efter kommer du inte ihåg någonting.");
+            Console.WriteLine("\nKolla i föremålet som ligger bredvid dig eller ställ dig upp. (kolla, stå upp)");
 
-        // Bad Structure-projektet  ------------------------------------------------------
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-         // ------------------------------------------------------------------------------
+            // Bad Structure-projektet  ------------------------------------------------------
+            string choice = Console.ReadLine();
+            choice = choice.ToLower();
+            // ------------------------------------------------------------------------------
 
-        if (choice == "1")
-        {
-            Console.WriteLine("Det verkar vara en ryggsäck med saker i.");
-            position = "checkBag";
+            if (choice == "kolla")
+            {
+                Console.WriteLine("Det verkar vara en ryggsäck med saker i.");
+                position = "checkBag"; //använda metoden bag() här?
+            }
+            else if (choice == "stå upp")
+            {
+                position = "standUp";
+            }
+            else
+            {
+                Console.WriteLine("Du har inget annat val.");
+                position = "start";
+            }
         }
-        else if (choice == "2")
-        {
-            position = "standUp";
-        }
-        else
-        {
-            Console.WriteLine("Du har inget annat val.");
-            position = "start";
-        }
+
         return position;
     }
 
     public static string BeachPlace(string position)
     {
-        
+        if (position == "standUp")
+        {
+            Console.WriteLine("Du står på en strand som sträcker sig så långt ögat kan se åt båda håll. Framför dig ser du havet möta horisonten. Bakom dig finns en djungel med höga träd.");
+        Console.WriteLine("Kolla i ryggsäcken, gå längs med stranden eller utforska djungeln (kolla, stranden, djungeln)");
+
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+
+        if (choice == "kolla")
+        {
+            // Bag();
+            position = "checkBag";
+        }
+        if (choice == "stranden")
+        {
+            position = "walkOnBeach";
+        }
+        if (choice == "djungeln")
+        {
+            position = "exploreDjungle";
+        }
+        }
+        if (position == "walkOnBeach")
+        {
+        Console.WriteLine("Du ser bara stranden framför dig, den verkar aldrig ta slut.Du fortsätter gå framåt.\nSplash\nEtt plötsligt ljud får dig att kolla dig omkring.");
+        Console.WriteLine("Kolla i ryggsäcken, undersök vart ljudet kom ifrån eler gå vidare (kolla, undersök, gå vidare)");
+
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+
+        if (choice == "kolla")
+        {
+            // Bag();
+            position = "checkBag";
+        }
+        if (choice == "undersök")
+        {
+            position = "exploreSound";
+        }
+        if (choice == "gå vidare")
+        {
+            position = "continueWalkOnBeach";
+        }
+        }
+
         
         return position;
     }
--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography; //för att listor ska fungera
 /* ----------------------------------------------------------------------------
     VARIABLER 
 -----------------------------------------------------------------------------*/
@@ -24,69 +25,36 @@ while (position != "leave")
         Bag();
         position = "start";
     }
+    // placera allt i Places.cs - inte massa if satser här
     if (position == "standUp")
     {
-        Console.WriteLine("Du står på en strand som sträcker sig så långt ögat kan se åt båda håll. Framför dig ser du havet möta horisonten. Bakom dig finns en djungel med höga träd.");
-        Console.WriteLine("1. Kolla i ryggsäcken\n2. Gå längs med stranden\n3. Gå och utforska djungeln");
-
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "1")
-        {
-            Bag();
-        }
-        if (choice == "2")
-        {
-            position = "walkOnBeach";
-        }
-        if (choice == "3")
-        {
-            position = "exploreDjungle";
-        }
+        position = Places.BeachPlace(position);
     }
     if (position == "walkOnBeach")
     {
-        Console.WriteLine("Du ser bara stranden framför dig, den verkar aldrig ta slut.Du fortsätter gå framåt.\nSplash\nEtt plötsligt ljud får dig att kolla dig omkring.");
-        Console.WriteLine("1. Kolla i ryggsäcken\n2. Undersök vart ljudet kom ifrån\n3. Gå vidare");
-
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "1")
-        {
-            Bag();
-        }
-        if (choice == "2")
-        {
-            position = "exploreSound";
-        }
-        if (choice == "3")
-        {
-            position = "continueWalkOnBeach";
-        }
+        position = Places.BeachPlace(position);
     }
     if (position == "continueWalkOnBeach")
     {
         Console.WriteLine("Efter några timmars promenerande märker du hur sanden börjar röra på sig under dig. Du lyckas dyka åt sidan precis innan något hoppar upp ur sanden. Det ser ut som en... jättekrabba.");
-        Console.WriteLine("1. Kolla i ryggsäcken\n2. Dra fram svärdet från ryggsäcken och slå krabban.\n3. Spring därifrån.");
+        Console.WriteLine("Kolla i ryggsäcken, dra fram svärdet från ryggsäcken och slå krabban eller spring därifrån (kolla, svärd, spring)");
 
         string choice = Console.ReadLine();
         choice = choice.ToLower();
 
-        if (choice == "1")
+        if (choice == "kolla")
         {
             Console.WriteLine("Du hinner inte kolla i ryggsäcken innan jättekrabban attackerar dig med sina stora klor. I dina sista stunder funderar du på vad du skulle ha kunnat göra bättre.");
             position = "leave";
         }
-        if (choice == "2")
+        if (choice == "svärd")
         {
             Console.WriteLine("Du drar fram ditt svärd");
             position = "fightCrab";
         }
-        if (choice == "3")
+        if (choice == "spring")
         {
-            Console.WriteLine("Du rusar därifrån, men upptäcker snabbt att bästa sättet att komma bort från krabban är att springa in i skogen.");
+            Console.WriteLine("Du rusar därifrån och upptäcker snabbt att bästa sättet att komma bort från krabban är att springa in i skogen.");
             position = "hideInDjungle";
         }
     }
@@ -114,23 +82,48 @@ while (position != "leave")
 -----------------------------------------------------------------------------*/
     if (position == "fightCrab")
     {
-        Console.WriteLine("Slå på krabbans huvud eller en av dess klor.");
+        // fight ska vara någon metod - hur?
+        Console.WriteLine("Slå på krabbans huvud eller en av dess klor. (huvud, klo)");
         Console.ReadLine();
+
+        int playerHP = 100;
+        int enemyHP = 100;
+
+        Random generator = new Random();
+
+        while (playerHP > 0 && enemyHP > 0)
+        {
+
+        }
+
         Fight();
     }
 
 Console.ReadLine();
 
-// ändra till en bool eller liknande
+// ändra till en bool eller liknande?
 static void Fight()
 {
-
+     
 }
 
 static void Bag()
 {
-    Console.WriteLine("Min väska innehåller:");
+    Console.WriteLine("Ryggsäcken innehåller:");
+    // string[] items = {"Sword", "Letter"};
+    // int i = 0;
+
+    // for (int i = 0; i < items.Length; i++)
+    // {
+    //     Console.WriteLine(items[i]);
+    // }
+
+    // Lista för att utrymmet i ryggsäcken inte ska vara begränsat
+    List<string> items = new List<string>() {"Svärd" , "Brev"};
+    
+    // för att enklare kunna lägga till nya saker och så att det skrivs ut tillsammans [0], [1], [2]
+    foreach (string item in items)
+    {
+        Console.WriteLine(item);
+    }
 }
-
-
-// List<string> items = new List<string>();
