23653bd8e652e79936ea356d7bc5f48074033977
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-03-20 10:57:49 +0100

    ändrat svarsalternativ igen, börjat på en metod för att hindra felsvar

--- Slutprojektet/Places.cs
+++ Slutprojektet/Places.cs
@@ -9,19 +9,19 @@ class Places
         if (position == "start")
         {
             Console.WriteLine("Du ser en massa blått när du öppnar ögonen. Det är troligtvis en himmel.\nDu minns inte varför du kollar på en himmel.\nNär du tänker efter kommer du inte ihåg någonting.");
-            Console.WriteLine("\nKolla i föremålet som ligger bredvid dig eller ställ dig upp. (kolla, stå upp)");
+            Console.WriteLine("\na) Kolla i föremålet som ligger bredvid dig\nb) Ställ dig upp");
 
             // Bad Structure-projektet  ------------------------------------------------------
             string choice = Console.ReadLine();
             choice = choice.ToLower();
             // ------------------------------------------------------------------------------
 
-            if (choice == "kolla")
+            if (choice == "a")
             {
                 Console.WriteLine("Det verkar vara en ryggsäck med saker i.");
                 position = "checkBag"; //använda metoden bag() här?
             }
-            else if (choice == "stå upp")
+            else if (choice == "b")
             {
                 position = "standUp";
             }
@@ -40,21 +40,21 @@ class Places
         if (position == "standUp")
         {
             Console.WriteLine("Du står på en strand som sträcker sig så långt ögat kan se åt båda håll. Framför dig ser du havet möta horisonten. Bakom dig finns en djungel med höga träd.");
-            Console.WriteLine("Kolla i ryggsäcken, gå längs med stranden eller utforska djungeln (kolla, stranden, djungeln)");
+            Console.WriteLine("a) Kolla i ryggsäcken\nb) Gå längs med stranden\nc) Utforska djungeln");
 
             string choice = Console.ReadLine();
             choice = choice.ToLower();
 
-            if (choice == "kolla")
+            if (choice == "a")
             {
                 // Bag();
                 position = "checkBag";
             }
-            if (choice == "stranden")
+            if (choice == "b")
             {
                 position = "walkOnBeach";
             }
-            if (choice == "djungeln")
+            if (choice == "c")
             {
                 position = "exploreDjungle";
             }
@@ -62,27 +62,125 @@ class Places
         if (position == "walkOnBeach")
         {
             Console.WriteLine("Du ser bara stranden framför dig, den verkar aldrig ta slut.Du fortsätter gå framåt.\nSplash\nEtt plötsligt ljud får dig att kolla dig omkring.");
-            Console.WriteLine("Kolla i ryggsäcken, undersök vart ljudet kom ifrån eler gå vidare (kolla, undersök, gå vidare)");
+            Console.WriteLine("a) Kolla i ryggsäcken\nb) Undersök vart ljudet kom ifrån\nc) Gå vidare");
 
             string choice = Console.ReadLine();
             choice = choice.ToLower();
 
-            if (choice == "kolla")
+            if (choice == "a")
             {
                 // Bag();
                 position = "checkBag";
             }
-            if (choice == "undersök")
+            if (choice == "b")
             {
                 position = "exploreSound";
             }
-            if (choice == "gå vidare")
+            if (choice == "c")
+            {
+                position = "continueWalkOnBeach";
+            }
+        }
+        if (position == "continueWalkOnBeach")
+    {
+        Console.WriteLine("Efter några timmars promenerande märker du hur sanden börjar röra på sig under dig. Du lyckas dyka åt sidan precis innan något hoppar upp ur sanden. Det ser ut som en... jättekrabba.");
+        Console.WriteLine("a) Kolla i ryggsäcken\nb) Dra fram svärdet från ryggsäcken och slå krabban\nc) Spring därifrån");
+
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+
+        if (choice == "a")
+        {
+            Console.WriteLine("Du hinner inte kolla i ryggsäcken innan jättekrabban attackerar dig med sina stora klor. I dina sista stunder funderar du på vad du skulle ha kunnat göra bättre.");
+            position = "leave";
+        }
+        if (choice == "b")
+        {
+            Console.WriteLine("Du drar fram ditt svärd");
+            position = "fightCrab";
+        }
+        if (choice == "c")
+        {
+            Console.WriteLine("Du rusar därifrån och upptäcker snabbt att bästa sättet att komma bort från krabban är att springa in i skogen.");
+            position = "hideInDjungle";
+        }
+    }
+        
+        return position;
+    }
+
+    public static string WaterPlace(string position)
+    {
+        if (position == "exploreSound")
+        {
+            Console.WriteLine("Ljudet kom från havet. Hmmm... Du ser något som glittrar under vatten ytan. \na) Kolla i ryggsäcken\nb) Plocka upp det glittrande föremålet\nc) Gå vidare");
+            
+            string choice = Console.ReadLine();
+            choice = choice.ToLower();
+
+            if (choice == "a")
+            {
+                // Bag();
+                position = "checkBag";
+            }
+            if (choice == "b")
+            {
+                position = "pickUp";
+            }
+            if (choice == "c")
             {
                 position = "continueWalkOnBeach";
             }
         }
 
+        if (position == "pickUp")
+        {
+            Console.WriteLine("Det är en silvrig sten som är formad som en fyrklöver. Du lägger den i ryggsäcken och fortsätter vidare längs med stranden.");
+            // items.Add("Sten");
+            position = "continueWalkOnBeach";
+        }
+
+        return position;
+    }
+
+    public static string DjunglePlace(string position)
+    {
+        if (position == "exploreDjungle")
+        {
+            Console.WriteLine("Du går in bland träden, hör fåglar som kvittrar och insekter som flyger förbi. Det är trångt att gå mellan träden, men du fortsätter framåt. Efter ett tag kommer du fram till en stor sten med vad ser ut som en dörr på.");
+            // Om spelaren har stenen: använd den som nyckel.
+            // Om spelaren inte har stenen: undersök och gå vidare. 
+            position = "continueInDjungle";
+        }
+
+        if (position == "hideInDjungle")
+    {
+        Console.WriteLine("Du springer in i djungeln och upptäcker att jättekrabban inte följer efter dig. Vilken lättnad för dig. För din egen säkerhet är det nog bäst att du fortsätter framåt i djungeln.");
+
+        position = "exploreDjungle";
+    }
+    if (position == "continueInDjungle")
+    {
+        Console.WriteLine("Du går förbi stenen och fortsätter framåt i djungeln. Plötsligt hör du ett skrik. Ljudet kom uppifrån. När du kollar upp ser du en stor apa som sitter i ett träd och kollar på dig. När era ögon möts ger apan ifrån sig ett liknande skrik och hoppar ner från trädet, rakt mot dig. \na) Kolla i ryggsäcken\nb) Dra fram svärdet\nc) Spring därifrån");
         
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+
+        if (choice == "a")
+        {
+            Console.WriteLine("Du hinner inte börja kolla i ryggsäcken innan apan greppar tag i dig, runt din hals, och klämmer livet ur dig");
+            position = "leave";
+        }
+        if (choice == "b")
+        {
+            
+        }
+        if (choice == "c")
+        {
+            Console.WriteLine("Du hinner vända dig och börja springa åt ett annat håll innan du blir träffad av något tungt som får dig att ramla och slå i huvudet på en sten. Innan allt blir svart ser du hur apan närmar dig.");
+            position = "wakeUp";
+        }
+    }
         return position;
     }
 }
--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic; //för att listor ska fungera
-using System.Security.Cryptography; 
+using System.Security.Cryptography;
+using System.Xml.Serialization;
 /* ----------------------------------------------------------------------------
     VARIABLER 
 -----------------------------------------------------------------------------*/
@@ -29,109 +30,27 @@ while (position != "leave")
         Bag();
         position = "start";
     }
-    // placera allt i Places.cs - inte massa if satser här
     if (position == "standUp")
     {
         position = Places.BeachPlace(position);
-    }
-    if (position == "walkOnBeach")
-    {
-        position = Places.BeachPlace(position);
-    }
-    if (position == "continueWalkOnBeach")
-    {
-        Console.WriteLine("Efter några timmars promenerande märker du hur sanden börjar röra på sig under dig. Du lyckas dyka åt sidan precis innan något hoppar upp ur sanden. Det ser ut som en... jättekrabba.");
-        Console.WriteLine("Kolla i ryggsäcken, dra fram svärdet från ryggsäcken och slå krabban eller spring därifrån (kolla, svärd, spring)");
-
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "kolla")
-        {
-            Console.WriteLine("Du hinner inte kolla i ryggsäcken innan jättekrabban attackerar dig med sina stora klor. I dina sista stunder funderar du på vad du skulle ha kunnat göra bättre.");
-            position = "leave";
-        }
-        if (choice == "svärd")
-        {
-            Console.WriteLine("Du drar fram ditt svärd");
-            position = "fightCrab";
-        }
-        if (choice == "spring")
-        {
-            Console.WriteLine("Du rusar därifrån och upptäcker snabbt att bästa sättet att komma bort från krabban är att springa in i skogen.");
-            position = "hideInDjungle";
-        }
     }
 /* ----------------------------------------------------------------------------
     POSITION: WATER 
 -----------------------------------------------------------------------------*/
     if (position == "exploreSound")
     {
-        Console.WriteLine("Ljudet kom från havet. Hmmm... Du ser något som glittrar under vatten ytan. Kolla i ryggsäcken, plocka upp det glittrande föremålet eller gå vidare. (kolla, plocka upp, gå vidare)");
-        
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "kolla")
-        {
-            Bag();
-        }
-        if (choice == "plocka upp")
-        {
-            position = "pickUp";
-        }
-        if (choice == "gå vidare")
-        {
-            position = "continueWalkOnBeach";
-        }
-    }
-    if (position == "pickUp")
-    {
-        Console.WriteLine("Det är en silvrig sten som är formad som en fyrklöver. Du lägger den i ryggsäcken och fortsätter vidare längs med stranden.");
-        // items.Add("Sten");
-        position = "continueWalkOnBeach";
+        position = Places.BeachPlace(position);
     }
 /* ----------------------------------------------------------------------------
     POSITION: DJUNGLE
 -----------------------------------------------------------------------------*/
     if (position == "exploreDjungle")
     {
-        Console.WriteLine("Du går in bland träden, hör fåglar som kvittrar och insekter som flyger förbi. Det är trångt att gå mellan träden, men du fortsätter framåt. Efter ett tag kommer du fram till en stor sten med vad ser ut som en dörr på.");
-        // Om spelaren har stenen: använd den som nyckel.
-        // Om spelaren inte har stenen: undersök och gå vidare. 
-        position = "continueInDjungle";
-    }
-    if (position == "hideInDjungle")
-    {
-        Console.WriteLine("Du springer in i djungeln och upptäcker att jättekrabban inte följer efter dig. Vilken lättnad för dig. För din egen säkerhet är det nog bäst att du fortsätter framåt i djungeln.");
-
-        position = "exploreDjungle";
-    }
-    if (position == "continueInDjungle")
-    {
-        Console.WriteLine("Du går förbi stenen och fortsätter framåt i djungeln. Plötsligt hör du en skrik. Ljudet kom uppifrån. När du kollar upp ser du en stor apa som sitter i ett träd och kollar på dig. När era ögon möts ger apan ifrån sig ett liknande skrik och hoppar ner från trädet, rakt mot dig. Kolla i ryggsäcken, dra fram svärdet eller spring därifrån (kolla, svärd, spring)");
-        
-        string choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "kolla")
-        {
-            Console.WriteLine("Du hinner inte börja kolla i ryggsäcken innan apan greppar tag i dig, runt din hals, och klämmer livet ur dig");
-            position = "leave";
-        }
-        if (choice == "svärd")
-        {
-
-        }
-        if (choice == "spring")
-        {
-            Console.WriteLine("Du hinner vända dig och börja springa åt ett annat håll innan du blir träffad av något tungt som får dig att ramla och slå i huvudet på en sten. Innan allt blir svart ser du hur apan närmar dig.");
-            position = "wakeUp";
-        }
+        position = Places.DjunglePlace(position);
     }
 }
 /* ----------------------------------------------------------------------------
-    POSITION: FIGHT
+    POSITION: HOUSE
 -----------------------------------------------------------------------------*/
     if (position == "wakeUp")
     {
@@ -145,16 +64,16 @@ while (position != "leave")
         Console.ReadLine();
         Console.WriteLine("...........................");
         Console.ReadLine();
-        Console.WriteLine("Du känner att du ligger på något mjukt och hör någon som stompar runt omkring dig. Ett några misslyckade försök lyckas du sakta öppna ögonen. ");
+        Console.WriteLine("Du känner att du ligger på något mjukt och hör någon som stompar runt omkring dig. Efter några misslyckade försök lyckas du sakta öppna ögonen. ");
         position = "leave";
-    }
+    }  
 /* ----------------------------------------------------------------------------
     POSITION: FIGHT
 -----------------------------------------------------------------------------*/
     if (position == "fightCrab")
     {
         // fight ska vara någon metod - hur?
-        Console.WriteLine("Slå på krabbans huvud eller en av dess klor. (huvud, klo)");
+        Console.WriteLine("a) Slå på krabbans huvud eller b) en av dess klor");
         Console.ReadLine();
 
         int playerHP = 100;
@@ -197,4 +116,12 @@ static void Bag()
     {
         Console.WriteLine(item);
     }
+}
+
+static void CheckChoice()
+{
+    // while (choice != "a" || choice != "b" || choice != "c")
+    // {
+
+    // }
 }
